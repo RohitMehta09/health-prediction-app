050a31406d80c3fc4d647c1413baeb404ae6f1c4
RohitMehta09 <mehtarohit.1600@gmail.com>
2025-05-07 22:10:47 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -66,7 +66,7 @@ if "username" not in st.session_state:
 if st.session_state.page == "prediction":
     st.sidebar.image(
         "https://plus.unsplash.com/premium_photo-1673953510107-d5aee40d80a7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
-        use_column_width=True
+        use_container_width=True
     )
 
 # Navigation logic
